5da5b57a2d2bb841a09422e730fb1d305b3ea4b3
Akram Hussein <akram@vektor.finance>
2022-03-08 11:01:22 -0300

    build(js): update test

--- js/tests/common/mod.rs
+++ js/tests/common/mod.rs
@@ -4,5 +4,5 @@ use vxl_wasm::parse;
 
 #[wasm_bindgen_test]
 pub fn parses() {
-  let _ = parse("COMMAND(10, ARG1, ARG2, ARG3, FALSE)");
+  let _ = parse("function.subfunction(10, false, \"hello\")");
 }
