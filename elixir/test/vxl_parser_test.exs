6220d16000cef0f72a2be82c36e004e55f421d39
Akram Hussein <akram@vektor.finance>
2022-06-03 14:30:36 -0300

    fix: re-order tests

--- elixir/test/vxl_parser_test.exs
+++ elixir/test/vxl_parser_test.exs
@@ -1,12 +1,6 @@
 defmodule VXLParserTest do
   use ExUnit.Case
 
-  test "parses function" do
-    assert VXLParser.parse("function.subfunction(1, false, \"hello\")") ==
-             {:ok,
-              "[{\"offset\":0,\"line\":1,\"column\":1,\"token\":{\"function\":{\"name\":{\"offset\":0,\"line\":1,\"column\":1,\"token\":{\"identifier\":\"function\"}},\"subfunction\":{\"offset\":9,\"line\":1,\"column\":10,\"token\":{\"identifier\":\"subfunction\"}},\"args\":[{\"offset\":21,\"line\":1,\"column\":22,\"token\":{\"number\":{\"int\":\"1\"}}},{\"offset\":24,\"line\":1,\"column\":25,\"token\":{\"boolean\":false}},{\"offset\":32,\"line\":1,\"column\":33,\"token\":{\"string\":\"hello\"}}]}}}]"}
-  end
-
   test "gets build info" do
     assert %{
              __struct__: VXL.BuildInfo,
@@ -15,5 +9,11 @@ defmodule VXLParserTest do
              git_sha: _,
              profile: _
            } = VXLParser.build_info()
+  end
+
+  test "parses function" do
+    assert VXLParser.parse("function.subfunction(1, false, \"hello\")") ==
+             {:ok,
+              "[{\"offset\":0,\"line\":1,\"column\":1,\"token\":{\"function\":{\"name\":{\"offset\":0,\"line\":1,\"column\":1,\"token\":{\"identifier\":\"function\"}},\"subfunction\":{\"offset\":9,\"line\":1,\"column\":10,\"token\":{\"identifier\":\"subfunction\"}},\"args\":[{\"offset\":21,\"line\":1,\"column\":22,\"token\":{\"number\":{\"int\":\"1\"}}},{\"offset\":24,\"line\":1,\"column\":25,\"token\":{\"boolean\":false}},{\"offset\":32,\"line\":1,\"column\":33,\"token\":{\"string\":\"hello\"}}]}}}]"}
   end
 end
