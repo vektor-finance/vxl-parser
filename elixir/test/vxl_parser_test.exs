9b3f69d5984e347f10dd50d7027c4e1d107bab95
Akram Hussein <akram@vektor.finance>
2022-06-03 14:32:08 -0300

    fix: add parse_error test

--- elixir/test/vxl_parser_test.exs
+++ elixir/test/vxl_parser_test.exs
@@ -11,9 +11,13 @@ defmodule VXLParserTest do
            } = VXLParser.build_info()
   end
 
-  test "parses function" do
+  test "successfully parses function" do
     assert VXLParser.parse("function.subfunction(1, false, \"hello\")") ==
              {:ok,
               "[{\"offset\":0,\"line\":1,\"column\":1,\"token\":{\"function\":{\"name\":{\"offset\":0,\"line\":1,\"column\":1,\"token\":{\"identifier\":\"function\"}},\"subfunction\":{\"offset\":9,\"line\":1,\"column\":10,\"token\":{\"identifier\":\"subfunction\"}},\"args\":[{\"offset\":21,\"line\":1,\"column\":22,\"token\":{\"number\":{\"int\":\"1\"}}},{\"offset\":24,\"line\":1,\"column\":25,\"token\":{\"boolean\":false}},{\"offset\":32,\"line\":1,\"column\":33,\"token\":{\"string\":\"hello\"}}]}}}]"}
+  end
+
+  test "fails to parses function" do
+    assert VXLParser.parse(";-123.") == {:error, :parse_error}
   end
 end
