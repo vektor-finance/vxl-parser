1da64abd72be3c2c525f20828b2e706b7512367c
Akram Hussein <akram@vektor.finance>
2022-06-03 14:30:14 -0300

    feat: add build_info elixir test

--- elixir/test/vxl_parser_test.exs
+++ elixir/test/vxl_parser_test.exs
@@ -6,4 +6,14 @@ defmodule VXLParserTest do
              {:ok,
               "[{\"offset\":0,\"line\":1,\"column\":1,\"token\":{\"function\":{\"name\":{\"offset\":0,\"line\":1,\"column\":1,\"token\":{\"identifier\":\"function\"}},\"subfunction\":{\"offset\":9,\"line\":1,\"column\":10,\"token\":{\"identifier\":\"subfunction\"}},\"args\":[{\"offset\":21,\"line\":1,\"column\":22,\"token\":{\"number\":{\"int\":\"1\"}}},{\"offset\":24,\"line\":1,\"column\":25,\"token\":{\"boolean\":false}},{\"offset\":32,\"line\":1,\"column\":33,\"token\":{\"string\":\"hello\"}}]}}}]"}
   end
+
+  test "gets build info" do
+    assert %{
+             __struct__: VXL.BuildInfo,
+             build_semver: _,
+             build_timestamp: _,
+             git_sha: _,
+             profile: _
+           } = VXLParser.build_info()
+  end
 end
