e2389868807a81b247cdd4c479fbca3c7e0f970c
Akram Hussein <akram@vektor.finance>
2023-11-27 19:19:20 -0300

    chore: include rustler precompiled checksum

---
+++ elixir/checksum-Elixir.VXLParser.exs
@@ -0,0 +1,12 @@
+%{
+  "libvxl_elixir-v0.0.33-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2e3f8216501de0e7c823703b6e084318ae086b421478f4e3baccf74b0ce86f7c",
+  "libvxl_elixir-v0.0.33-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:1902fe5d65ab96361ba5e9353e0823837f465f179e8033ffde0ada4918d8743a",
+  "libvxl_elixir-v0.0.33-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:6819f91133abc234241c2ea82b2642f9f579c1dd05fda17b9695ed7f6b6dab64",
+  "libvxl_elixir-v0.0.33-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:9b0bca6837fc385d6819ead7ae3bc1e2b6d1789f780fe4963acd76b024552899",
+  "libvxl_elixir-v0.0.33-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:b6e4a4e491c823364e40cffd2f52a2ffcff772d2a3b9cd34e99ab6ba13c2c7b7",
+  "libvxl_elixir-v0.0.33-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:c56945fbf012847f45a44120fb5302bda20f16a095b3e8276a2d4054edcf5031",
+  "libvxl_elixir-v0.0.33-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:61dd14d90df06a6e46197ed2545f54b0236fbceb3800249182893feee4ab4444",
+  "libvxl_elixir-v0.0.33-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:f40f2c08adf89dec194b9e3782609fd898fcdd42f9f13511eb890e128661775d",
+  "vxl_elixir-v0.0.33-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:b554c5817343c175b3a89564ba95f84aa6536b6e31cc5e120bb02d31ad731d8f",
+  "vxl_elixir-v0.0.33-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:34c32a04a972073f5ae7bb3177d9f79f74d77786024e884fb986da357eba1062",
+}
